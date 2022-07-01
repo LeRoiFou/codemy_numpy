07531a97eb756271d57fb2d2cc30b1b706c3ceec
Laurent REYNAUD <72187493+LeRoiFou@users.noreply.github.com>
2022-07-01 12:46:11 +0200

    Add files via upload

---
+++ index005_shape.py
@@ -0,0 +1,36 @@
+"""
+Cours : Shape and Reshape Numpy Arrays
+Lien : https://www.youtube.com/watch?v=OryO301RmX8
+
+Shape comme dans pandas !!!
+
+Éditeur : Laurent Reynaud
+Date : 01-07-22
+"""
+
+import numpy as np
+
+# Create 1-D Numpy Array and Get Shape
+np1 = np.array([1,2,3,4,5,6,7,8,9,10,11,12])
+print(np1)
+print(np1.shape)
+
+# Create 2-D Array and get Shape (rows, columns)
+# np2 = np.array([[1,2,3,4,5,6], [7,8,9,10,11,12]])
+# print(np2)
+# print(np2.shape)
+
+# Reshape 2-D
+# np3 = np1.reshape(3,4)
+# print(np3)
+# print(np3.shape)
+
+# Reshape 3-D
+np4 = np1.reshape(2,3,2)
+print(np4)
+print(np4.shape)
+
+# Flatten to 1-D
+np5 = np4.reshape(-1)
+print(np5)
+print(np5.shape)
